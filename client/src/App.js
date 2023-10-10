8036b12f343d821c418733939eabef0472cf83fb
thili-perera <84263208+thili-perera@users.noreply.github.com>
2023-10-10 10:44:43 +0530

    inital

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,54 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      test
-    </div>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://media.istockphoto.com/id/114288673/photo/car-production-line.jpg?s=612x612&w=0&k=20&c=AvZmruf6iXe9YVebcxEWe7NKo-91LB_r8pLEDLKA0Go="></img>
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup comming later this year</h2>
+          <p className="info">
+            <a className="author">Thilini Perera</a>
+            <time>2023-10-09 16:24</time>
+          </p>
+          <p className="summary">In linguistics and grammar, a sentence is a linguistic expression, such as the English example "The quick brown fox jumps over the lazy dog." In traditional grammar it is typically defined as a string of words that expresses a complete thought, or as a unit consisting of a subject and predicate.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://media.istockphoto.com/id/114288673/photo/car-production-line.jpg?s=612x612&w=0&k=20&c=AvZmruf6iXe9YVebcxEWe7NKo-91LB_r8pLEDLKA0Go="></img>
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup comming later this year</h2>
+          <p className="info">
+            <a className="author">Thilini Perera</a>
+            <time>2023-10-09 16:24</time>
+          </p>
+          <p>In linguistics and grammar, a sentence is a linguistic expression, such as the English example "The quick brown fox jumps over the lazy dog." In traditional grammar it is typically defined as a string of words that expresses a complete thought, or as a unit consisting of a subject and predicate.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://media.istockphoto.com/id/114288673/photo/car-production-line.jpg?s=612x612&w=0&k=20&c=AvZmruf6iXe9YVebcxEWe7NKo-91LB_r8pLEDLKA0Go="></img>
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup comming later this year</h2>
+          <p className="info">
+            <a className="author">Thilini Perera</a>
+            <time>2023-10-09 16:24</time>
+          </p>
+          <p>In linguistics and grammar, a sentence is a linguistic expression, such as the English example "The quick brown fox jumps over the lazy dog." In traditional grammar it is typically defined as a string of words that expresses a complete thought, or as a unit consisting of a subject and predicate.</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
